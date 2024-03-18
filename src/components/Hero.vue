a95759fa506f4c4c7ce04a9da72e135803994013
INTRA\z2220020 <tomoya.imamura@ctc-g.co.jp>
2024-03-18 15:29:33 +0900

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center hero">
+    <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
+    <h1 class="mb-4">Vue.js Sample Project</h1>
+    <p class="lead">
+      これは
+      <a
+        href="https://vuejs.org"
+      >Vue.js</a>を使ったサンプルSPAです
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeroContent"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <hr />
+    <hero />
+    <hr />
+    <home-content />
+  </div>
+</template>
+
+<script lang="ts">
+import Hero from "../components/Hero.vue";
+import HomeContent from "../components/HomeContent.vue";
+import Profile from "./Profile.vue";
+
+export default {
+  name: "home-view",
+  components: {
+    Hero,
+    HomeContent,
+    Profile,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.next-steps .fa-link {
+    margin-right: 5px;
+}
+</style>
